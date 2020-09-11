aeb87942e5a8d4a388aa99864165485127b7dfd3
mkwon <developer.mkwon@gmail.com>
2020-09-11 23:04:16 +0900

    add device size util

---
+++ lib/utils/device_util.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+double getDeviceWidth(BuildContext context) => MediaQuery.of(context).size.width;
+double getDeviceHeight(BuildContext context) => MediaQuery.of(context).size.height;
+double getStatusBarHeight(BuildContext context) => MediaQuery.of(context).padding.top;
