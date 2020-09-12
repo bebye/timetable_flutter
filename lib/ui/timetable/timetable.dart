7c0bb6fe90ad3440a95c04dec2f8ceeada685bbe
mkwon <developer.mkwon@gmail.com>
2020-09-12 19:42:43 +0900

    update timtetable

--- lib/ui/timetable/timetable.dart
+++ lib/ui/timetable/timetable.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:timetable_flutter/ui/timetable/timetable_content.dart';
 import 'package:timetable_flutter/ui/timetable/timetable_header.dart';
+import 'package:timetable_flutter/utils/device_util.dart';
 
-class Timetable extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => _TimetableState();
-}
+class Timetable extends StatelessWidget {
+  static double cellWidth;
+  static double cellHeight = 80;
+  static int cellSize = 6;
+  static double headerHeight = 30;
 
-class _TimetableState extends State<Timetable> {
   @override
   Widget build(BuildContext context) {
-    return TimetableHeader();
+    cellWidth = getDeviceWidth(context) / cellSize;
+    return Stack(
+      children: [
+        SingleChildScrollView(
+          padding: EdgeInsets.only(
+            top: headerHeight,
+          ),
+          child: TimetableContent(),
+        ),
+        TimetableHeader(),
+      ],
+    );
   }
 }
