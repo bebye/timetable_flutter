0f0dca8265cc946b043578617374a74245e30d64
mkwon <developer.mkwon@gmail.com>
2020-09-12 19:41:32 +0900

    add build widget method

--- lib/utils/widget_util.dart
+++ lib/utils/widget_util.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:timetable_flutter/common/style.dart';
+import 'package:timetable_flutter/common/style_colors.dart';
 
 Widget buildText(String text, double size, Color color,
-        {FontWeight weight = FontWeight.normal}) =>
+        {TextAlign align = TextAlign.center,
+        FontWeight weight = FontWeight.normal}) =>
     Text(
       text,
+      textAlign: align,
       style: TextStyle(fontSize: size, color: color, fontWeight: weight),
     );
 
-BoxDecoration addBottomBorder(double width, {Color color = primaryColor}) => BoxDecoration(
+BoxDecoration addBottomBorder(
+        {double width = 1.0,
+        Color bgColor = Colors.transparent,
+        Color borderColor = colorBorderSecondary}) =>
+    BoxDecoration(
+      color: bgColor,
       border: Border(
-          bottom: BorderSide(
-        color: color,
-        width: width,
-      )),
+          bottom: buildBorderSide(width: width, borderColor: borderColor)),
+    );
+
+BorderSide buildBorderSide(
+        {double width = 1.0, Color borderColor = colorBorderSecondary}) =>
+    BorderSide(
+      color: borderColor,
+      width: width,
     );
