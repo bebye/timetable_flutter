b8446b974c940b7ad82032a9098f095f9aa5a815
mkwon <developer.mkwon@gmail.com>
2020-09-12 19:40:17 +0900

    add color style guide

---
+++ lib/common/style_colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+// Default
+const colorPrimary = Colors.teal;
+const colorSecondary = Colors.white;
+
+// Background
+const colorBgOnPrimary = Colors.white;
+const colorBgOnSecondary = Colors.teal;
+
+// Text
+const colorTextOnPrimary = colorSecondary;
+const colorTextSubOnPrimary = colorSecondary;
+const colorTextOnSecondary = colorPrimary;
+const colorTextSubOnSecondary = Colors.black45;
+
+// Border
+const colorBorderPrimary = colorPrimary;
+const colorBorderSecondary = Colors.black12;
